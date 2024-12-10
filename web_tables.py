2457d2b1db6253f72ae2d15dbb0f739a1dd72e36
Rival Muhammad Sayid <rivalmsayid@gmail.com>
2024-12-10 21:41:38 +0700

    add web tables

--- web_tables.py
+++ web_tables.py
@@ -11,7 +11,6 @@ driver.maximize_window()
 addButton = driver.find_element(By.XPATH, "/html/body/main/div/div/div[2]/form/div[1]/span[1]/button").click()
 time.sleep(10)
 
-#REGISTER FORM
 input_firstName = driver.find_element(By.ID,"firstname").send_keys("Rival")
 input_lastName = driver.find_element(By.ID,"lastname").send_keys("Muhammad")
 input_email = driver.find_element(By.ID,"email").send_keys("rivalmuhammad@gmail.com")
@@ -20,3 +19,21 @@ input_salary = driver.find_element(By.ID,"salary").send_keys("5,000,000,000,000,
 input_deparment = driver.find_element(By.ID,"deparment").send_keys("Technology")
 buttonLogin = driver.find_element(By.XPATH,"//input[@type='submit']").click()
 time.sleep(10)
+
+#'REGISTRATION FORM - DELETE USER
+deleteUser = driver.find_element(By.XPATH,"/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[1]/td[7]/a[2]").click()
+time.sleep(10)
+
+# #REGISTRATION FORM - EDIT USER
+editUser = driver.find_element(By.XPATH,"/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[1]/td[7]/a[1]").click()
+time.sleep(10)
+#' firstName = driver.find_element(By.XPATH,"(//input[@id='firstname'])[2]")
+# firstName.send_keys("All")
+# time.sleep(10)
+# lastName = driver.find_element(By.ID,"lastname").send_keys("Aulia")
+# email = driver.find_element(By.ID,"email").send_keys("aurelaulia@gmail.com")
+# age = driver.find_element(By.ID,"age").send_keys("25 Tahun")
+# salary = driver.find_element(By.ID,"salary").send_keys("5,000,000,000,000,000,000,000,000")
+# deparment = driver.find_element(By.ID,"deparment").send_keys("Technology")
+# Login = driver.find_element(By.XPATH,"//input[@type='submit']").click()
+# time.sleep(10)
