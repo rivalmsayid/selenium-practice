db98c627bb822309d0ecb2b24090c9036f41d4aa
Rival Muhammad Sayid <rivalmsayid@gmail.com>
2024-12-13 22:46:35 +0700

    add accordion widget

---
+++ accordion.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.tutorialspoint.com/selenium/practice/accordion.php")
+driver.maximize_window()
+
+clickDropdown1 = driver.find_element(By.XPATH,"//*[@id='headingTwentyOne']/button").click()
+time.sleep(10)
+clickDropdown2 = driver.find_element(By.XPATH,"//*[@id='headingTwentyTwo']/button").click()
+time.sleep(10)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+clickDropdown3 = driver.find_element(By.XPATH,"//*[@id='headingTwentyThree']/button").click()
+time.sleep(10)
+driver.quit()
